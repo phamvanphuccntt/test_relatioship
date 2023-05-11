29e4d69773c4529ab3229c0b58b959ef7cac8810
PhucPV <phamvanphuccntt@gmail.com>
2023-05-11 22:44:29 +0700

    create

---
+++ src/main/java/com/example/app/ProductManages1Application.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManages1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManages1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/code/java/ProductManages1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.code.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManages1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
